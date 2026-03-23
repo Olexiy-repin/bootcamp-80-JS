859f7cbc7225998194fcbd28504d6a6ecfde2475
Olexiy-repin <repinlyoha@gmail.com>
2026-03-23 12:49:24 +0200

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,32 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let та const
+ * - Перевизначення значення
+ * - Найменування змінних
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Типи даних: number, string, boolean, null, undefined, BigInt, Symbol
+ * - Оператор typeof
+ */
+
+// let pricePerItem = 1000;
+
+// console.log(pricePerItem);
+
+// pricePerItem = 800;
+
+// console.log(pricePerItem);
+
+// alert('Hello world!');
+
+// const firstName = prompt('Your first name');
+
+// console.log(firstName);
+
+// const num = 10;
+// const str = '10';
+
+// console.log(typeof num);
+// console.log(typeof str);
---
+++ js/03-operators.js
@@ -0,0 +1,54 @@
+/*
+ * Арифметичні операції
+ * - Оператори +, -, *, /, %, **
+ * - Комбіновані оператори
+ */
+
+/*
+ TODO: Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+ */
+// const apples = 135;
+// const grapes = 47;
+
+// const total = apples + grapes;
+
+// console.log(total);
+
+// const diff = apples - grapes;
+
+// console.log(diff);
+
+/*
+ TODO: Заміни вираз перевизначення комбінованим оператором.
+ */
+//~ Без комбінованого оператора
+// let students = 100;
+
+// students = students + 50;
+
+// console.log(students);
+
+//~ З комбінованим оператором
+// let students = 100;
+
+// students += 50;
+
+// console.log(students);
+
+/*
+ TODO: Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+*/
+// const result = 108 + (223 - 2) * 5;
+
+// console.log(result);
+
+/*
+  TODO: Дано 90 хвилин, дізнайтеся скільки в цьому числі міститься годин і хвилин.
+  TODO: Результат виведіть у консоль.
+*/
+// const totalMinutes = 90;
+// const hours = Math.trunc(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+
+// console.log(hours);
+// console.log(minutes);
