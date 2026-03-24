06750187c6b0a281cba1a04554919e6f62b6b349
Olexiy-repin <repinlyoha@gmail.com>
2026-03-24 12:56:32 +0200

    classwork

---
+++ js/03-numbers.js
@@ -0,0 +1,49 @@
+/*
+ * Арифметичні функції
+ *
+ * - Number.parseInt
+ * - Number.parseFloat
+ * - Math
+ */
+
+/* 
+  TODO: Попросіть користувача ввести число.
+  TODO: Виведіть результат у консоль.
+  TODO: Приведіть рядок до числа.
+*/
+// const userInput = Number(prompt('Введіть число!'));
+// const userInput = +prompt('Введіть число!');
+
+// console.log(userInput);
+
+// TODO: Дізнайтесь числове значення ширини елемента.
+// let elementWidth = '50px';
+
+// elementWidth = Number.parseInt(elementWidth);
+
+// console.log('elementWidth =', elementWidth);
+
+// TODO: Дізнайтесь числове значення висоти елемента.
+// let elementHeight = '200.74px';
+
+// elementHeight = Number.parseFloat(elementHeight);
+
+// console.log('elementHeight =', elementHeight);
+
+// TODO: Попросіть користувача ввести два числа, складіть їх та виведіть результат у консоль.
+// const numA = Number(prompt('Введіть перше число'));
+// const numB = Number(prompt('Введіть друге число'));
+
+// const result = numA + numB;
+
+// console.log('result =', result);
+
+/*
+ TODO: Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д. значення змінної value.
+ TODO: Використовуй методи Math.floor(), Math.ceil() та Math.round(), Math.trunc().
+ */
+// let value = -27.1;
+
+// value = Math.floor(value);
+
+// console.log('value =', value);
